aa1c4925b015f7e9ff542d726fbe04c2b5821d10
ricardo_fernandes <ricardo.fernandes@inapa.com>
2019-04-11 20:48:22 +0100

    Add and Configure toaster (for beautifull alerts) | Add auth | Create and Configure DB | run migrate

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // demo toastr
+        toastr()->warning('Mensagem de Teste!', 'Titulo da Mensagem', ["timeOut" => "3000"])->success('BEM-VINDO');
+        return view('home');
+    }
+}
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,6 +12,8 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
+
+    window.toastr = require('toastr');
 } catch (e) {}
 
 /**
